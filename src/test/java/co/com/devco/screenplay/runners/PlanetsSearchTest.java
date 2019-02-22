6094e2dc4ec09990a4ad783c0adae73ac8ad3c5c
Jennifer Perez Bedoya <jenniferperezb@gmail.com>
2019-02-22 09:43:04 -0500

    Correccion en cofnfiguracion base

--- src/test/java/co/com/devco/screenplay/runners/PlanetsSearchTest.java
+++ src/test/java/co/com/devco/screenplay/runners/PlanetsSearchTest.java
@@ -4,8 +4,12 @@ import cucumber.api.CucumberOptions;
 import net.serenitybdd.cucumber.CucumberWithSerenity;
 import org.junit.runner.RunWith;
 
+import static cucumber.api.SnippetType.CAMELCASE;
+
 @RunWith(CucumberWithSerenity.class)
 @CucumberOptions(plugin = {"pretty"},
-                 features = "classpath:features")
+        features = "classpath:features",
+        glue = {"co.com.devco.screenplay.stepdefinitions"},
+        snippets = CAMELCASE)
 public class PlanetsSearchTest {
 }
